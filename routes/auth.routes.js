8472ee5f3e149d7a2ece037980ba0b93e9d83e34
Weslley Santos <weslleyrcs@icloud.com>
2026-02-06 10:40:56 +0100

    two user created

--- routes/auth.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { login } = require("../controllers/auth.controllers");
-
-router.post("/login", login);
-
-module.exports = router;
