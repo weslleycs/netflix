42c963832effced5de94ae6673d32b7f6438d76e
Weslley Santos <weslleyrcs@icloud.com>
2026-02-18 17:26:32 +0100

    feat: frontend setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-black text-white flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-red-500">Project Netflix</h1>
+    </div>
+  );
+}
